4561793c79cca3dbe48016aa7eb29bb5321b377f
softwinner813 <winner813@protonmail.com>
2022-08-03 06:34:00 -0700

    Add MVC & Layout

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Scratch.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        //public String Index()
+        {
+            ViewData["title"] = "Home | Index";
+            return View();
+            //return "Home page Started";
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+
+// Use MVC
+services.AddMvc();
+
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if(app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+
+// App Routing
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{ 
+    endpoints.MapDefaultControllerRoute(); 
+});
+
+//app.MapGet("/", () => "Hello World!");
 
 app.Run();
